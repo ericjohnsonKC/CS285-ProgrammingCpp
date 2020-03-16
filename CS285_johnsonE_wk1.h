b0d2209952059e6cbd4eba123bd0b38a7fb9d902
ericj <ericjohnson.kc@gmail.com>
2020-03-16 14:53:32 -0500

    Basic start of the assignment

---
+++ CS285_johnsonE_wk1.h
@@ -0,0 +1,35 @@
+/*  Author: Eric Johnson
+    Date: 3/16/2020
+    Grantham University
+    Student ID: 150737
+    CS285 Programming in C++
+    Week 1 assignment 
+*/
+
+#include <iostream>
+using namespace std;
+
+class BankAccount{
+    public:
+
+    /*
+    deposit()
+    withdraw()
+    getInterest()
+    updateBalance()
+    print()
+    getAccountNumber()
+    getAccountHolderName()
+    getAccountType()
+    getBalance()
+    getInterestRate()
+    */
+
+    private:
+        string accountHolderName;
+        int accountNumber;
+        string accountTyp;
+        double accountBalance;
+        double interestRate;
+
+};
---
+++ CS285_johnsonE_wk1_main.cpp
@@ -0,0 +1,9 @@
+/*  Author: Eric Johnson
+    Date: 3/16/2020
+    Grantham University
+    Student ID: 150737
+    CS285 Programming in C++
+    Week 1 assignment */
+
+    #include <iostream>
+    using namespace std;
