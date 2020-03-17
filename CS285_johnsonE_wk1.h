1e8db43acf057dd0d71ca60b334408f300dc58a5
eric <ericjohnson.kc@gmail.com>
2020-03-16 22:53:04 -0500

    I've not debugged any of the 3 files yet.  Reached a stopping point.

--- CS285_johnsonE_wk1.h
+++ CS285_johnsonE_wk1.h
@@ -1,35 +1,33 @@
-/*  Author: Eric Johnson
-    Date: 3/16/2020
-    Grantham University
-    Student ID: 150737
-    CS285 Programming in C++
-    Week 1 assignment 
+/*  Author: Eric Johnson, Date: 3/16/2020
+    Grantham University, Student ID: 150737
+    CS285 Programming in C++ Week 1 assignment 
 */
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 class BankAccount{
     public:
-
-    /*
-    deposit()
-    withdraw()
-    getInterest()
-    updateBalance()
-    print()
-    getAccountNumber()
-    getAccountHolderName()
-    getAccountType()
-    getBalance()
-    getInterestRate()
-    */
-
+        void deposit();
+        void withdraw();
+        double getInterest() const;
+        void print() const;
+        int getAccountNumber() const;
+        string getAccountType() const;
+        double getBalance() const;
+        double getInterestRate() const;
+        BankAccount(string, string, double, double);
+        BankAccount();
+        
     private:
         string accountHolderName;
         int accountNumber;
-        string accountTyp;
-        double accountBalance;
+        string accountType;
+        double balance;
         double interestRate;
+        static int accountNumber;
+
+        void updateBalance(double);
 
 };
--- CS285_johnsonE_wk1_imp.cpp
+++ CS285_johnsonE_wk1_imp.cpp
@@ -1,10 +1,113 @@
-/*  Author: Eric Johnson
-    Date: 3/16/2020
-    Grantham University
-    Student ID: 150737
-    CS285 Programming in C++
-    Week 1 assignment */
+/*  Author: Eric Johnson, Date: 3/16/2020
+    Grantham University, Student ID: 150737
+    CS285 Programming in C++ Week 1 assignment 
+*/
 
-    #include <iostream>
+#include <iostream>
+#include <string>
+#include "CS285_johnsonE_wk1.h"
+
 using namespace std;
 
+/*
+        void deposit(double);
+        void withdraw(double);
+        double getInterest() const;
+        double updateBalance();
+        void print() const;
+        int getAccountNumber() const;
+        string getAccountType() const;
+        double getBalance() const;
+        double getInterestRate() const;
+        BankAccount(string, string, double, double);
+        BankAccount();
+*/
+
+void BankAccount::deposit()
+{
+    double depositAmount = 0;
+    
+    cout << "Enter deposit amount: ";
+    cin >> depositAmount;
+    cout << endl;
+
+    if (depositAmount >= 0)
+        updateBalance(depositAmount);
+    else
+        cout << "Please enter a valid amount." << endl;    
+}
+
+void BankAccount::withdraw()
+{
+    double withdrawAmount = 0;
+
+    cout << "Enter withdraw amount: ";
+    cin >> withdrawAmount;
+    cout << endl;
+
+    if (withdrawAmount >= 0)
+        withdrawAmount *= -1;
+        updateBalance(withdrawAmount);
+    else
+        cout << "Please enter a valid amount." << endl;   
+}
+
+double BankAccount::getInterest()
+{
+    double interest = balance * interestRate
+    cout << "Interest: $" + interest << endl;
+}
+
+void BankAccount::updateBalance(double debit)
+{
+    balance += debit;
+}
+
+void BankAccount::print()
+{
+    cout << "Account holder's name: " + accountHolderName << endl;
+    cout << "Account type: " + accountType << endl;
+    cout << "Balance: " + balance << endl;
+    cout << "Interest rate: " + interestRate << endl;    
+}
+
+int BankAccount::getAccountNumber()
+{
+    return accountNumber;
+}
+
+string BankAccount::getAccountHolderName()
+{
+    return accountHolderName;
+}
+
+string BankAccount::getAccountType()
+{
+    return accountType;
+}
+
+double BankAccount::getBalance()
+{
+    return balance;
+}
+
+double BankAccount::getInterestRate()
+{
+    return interestRate;
+}
+
+BankAccount(string name, string type, double bal, double rate)
+{
+    accountHolderName = name;
+    accountType = type;
+    balance = bal;
+    interestRate = rate;
+}
+
+BankAccount()
+{
+    accountHolderName = "";
+    accountType = "";
+    balance = 0;
+    interestRate = 0;
+}
