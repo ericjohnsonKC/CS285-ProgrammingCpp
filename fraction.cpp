5b4e2de266b57fab8345cfcf45b5230973199697
eric <ericjohnson.kc@gmail.com>
2020-04-09 22:52:58 -0500

    Made some changes to file names, having trouble linking the files now.  Getting errors that my functions are undefined, still debugging.

--- fraction.cpp
+++ fraction.cpp
@@ -4,139 +4,6 @@
     CS285 Advanced Programming in C++
     Week 4 Assignment
 
-    This is a template class header file
-    for creating fractions and doing 
-    arithmetic on them.
-*/
-
-#ifndef fraction_h
-#define fraction_h
-
-#include <iostream>
-
-using namespace std;
-
-template<typename T>
-class Fraction
-{
-    friend ostream& operator<< (ostream& osObj, Fraction<T>& thisFraction);
-        // Overloads the output stream insertion operator.  
-        // The output is in the format:
-        // numerator/denominator
-        // Ex: 1/2 or 2/3, etc.
-    
-    friend istream& operator>> (istream& isObj, Fraction<T>& thisFraction);
-        // Overloads the input stream insertion operator.
-        // The input is taken from the user in the format:
-        // numerator/denominator
-        // Ex: 1/2 or 2/3, etc.
-    
-    public:
-        void setValues(T& numeratorParam, T& denominatorParam);
-            // Sets the values of the member private
-            // variables numerator and denominator.
-
-        T getNumerator() const;
-            // Returns the value of numerator.
-
-        T getDenominator() const;
-            // Returns the value of denominator.
-
-        Fraction operator++ ();
-            // Pre-increment operator.
-            // Increments the value of the 
-            // numerator by 1 BEFORE using
-            // the object in an expression.
-
-        Fraction operator++ (T dummyParameter);
-            // Post-increment operator.
-            // Increments the value of the 
-            // numerator by 1 AFTER using 
-            // the object in an expression.
-
-        Fraction operator-- ();
-            // Pre-decrement operator.
-            // Decrements the value of the 
-            // numerator by 1 BEFORE using
-            // the object in an expression.
-
-        Fraction operator-- (T dummyParameter);
-            // Post-decrement operator.
-            // Decrements the value of the 
-            // numerator by 1 AFTER using 
-            // the object in an expression.
-
-        bool operator== (const Fraction& otherFraction) const;
-            // Overloads the equality operator.
-            // Returns true the fractions are 
-            // EQUAL in reduced form.
-
-        bool operator!= (const Fraction& otherFraction) const;
-            // Overload the non-equality operator.
-            // Returns true if the fraction are
-            // NOT EQUAL in reduced form.
-
-        bool operator<= (const Fraction& otherFraction) const;
-            // Overloads the less than or equal operator.
-            // Returns true if less than or equal to the 
-            // other fraction. 
-
-        bool operator< (const Fraction& otherFraction) const;
-            // Overloads the less than operator.
-            // Returns true if less than the 
-            // other fraction.
-
-        bool operator>= (const Fraction& otherFraction) const;
-            // Overloads the greater than or equal operator.
-            // Returns true if greater than the 
-            // other fraction.
-
-        bool operator> (const Fraction& otherFraction) const;
-            // Overloads the greater than operator.
-            // Returns true if greater than the 
-            // other fraction.
-
-        Fraction operator+ (const Fraction& otherFraction) const;
-            // Overloads the addition operator.
-            // Calculates a common denominator, then 
-            // adds the numerators.
-
-        Fraction operator- (const Fraction& otherFraction) const;
-            // Overloads the subtraction operator.
-            // Calculates a common denominator, then 
-            // subtracts the otherFraction numerator 
-            // from the numerator.
-
-        Fraction operator* (const Fraction& otherFraction) const;
-            // Overloads the multiplication operator.
-            // Multiplies the numerators and multiplies
-            // the denominators.
-
-        Fraction operator/ (const Fraction& otherFraction) const;
-            // Overloads the division operator.
-            // Multiplies the calling Fraction object 
-            // by the inverse of the otherFraction object.
-        
-        Fraction (T numeratorParam = 0, T denominatorParam = 1);
-            // Constructor with default values.
-   
-    private:
-        T numerator;
-        T denominator;
-};
-
-#endif
-// End of header file
-//----------------------------------------------------------------
-
-
-
-/*  Author: Eric Johnson
-    Date: 4/7/2020
-    Grantham University
-    CS285 Advanced Programming in C++
-    Week 4 Assignment
-
     This is a template class implementation 
     file for creating fractions and doing 
     arithmetic on them.  It has been moved
@@ -144,33 +11,14 @@ class Fraction
     as a class template.
 */
 
-//#include "fraction.h"
-    // Commented out after moving implementation
-    // into the header file.
-//#include <iostream>
-    // Commented out for same reason as above.
+#include "fraction.h"
+   
+#include <iostream>
+    
+using namespace std;
+    
 
-//using namespace std;
-    // Commented out for same reason as above.
 
-template<typename T>
-ostream& operator<< (ostream& osObj, Fraction<T>& thisFraction)
-{
-    osObj << thisFraction.numerator << "/" << thisFraction.denominator;
-    
-    return osObj;
-}
-template<typename T>
-istream& operator>> (istream& isObj, Fraction<T>& thisFraction)
-{
-    char ch;
-    
-    isObj >> thisFraction.numerator;
-    isObj.get(ch);
-    isObj >> thisFraction.denominator;
-    
-    return isObj;
-}
 template <typename T>
 void Fraction<T>::setValues(T& numeratorParam, T& denominatorParam)
 {
@@ -193,17 +41,17 @@ T Fraction<T>::getDenominator() const
 template <typename T>
 Fraction<T> Fraction<T>::operator++ ()
 {
-    ++numerator;
+    numerator += 1;
 
     return *this;
 }
 
 template <typename T>
-Fraction<T> Fraction<T>::operator++ (T dummyParameter)
+Fraction<T> Fraction<T>::operator++ (int dummyParameter)
 {
     Fraction temp = *this;
 
-    numerator++;
+    numerator += 1;
 
     return temp;
 
@@ -212,17 +60,17 @@ Fraction<T> Fraction<T>::operator++ (T dummyParameter)
 template <typename T>
 Fraction<T> Fraction<T>::operator-- ()
 {
-    numerator--;
+    numerator -= 1;
 
     return *this;
 }
 
 template <typename T>
-Fraction<T> Fraction<T>::operator-- (T dummyParameter)
+Fraction<T> Fraction<T>::operator-- (int dummyParameter)
 {
     Fraction temp = *this;
 
-    numerator--;
+    numerator -=1;
 
     return temp;
 }
--- fraction_Test.cpp
+++ fraction_Test.cpp
@@ -9,7 +9,7 @@
     arithmetic on them.
 */
 
-#include "fraction.cpp"
+#include "fraction.h"
 
 #include <iostream>
 
@@ -17,15 +17,23 @@ using namespace std;
 
 int main()
 {
+
+    /*  This main test file is split in two identical
+        parts.  The first run uses integers, the second
+        part is basically a copy paste using doubles.
+    */
+
+   // Part one, using integers:
+
     // Test the constructors:
-    Fraction frac_1(1, 2);
-    Fraction frac_2(2, 3);
+    Fraction<int> frac_1(1, 2);
+    Fraction<int> frac_2(2, 3);
 
     cout << "Fraction #1: " << frac_1 << endl;
     cout << "Fraction #2: " << frac_2 << endl << endl;
 
-    Fraction frac_3;
-    cout << "Enter a fraction: ";
+    Fraction<int> frac_3;
+    cout << "Enter a fraction using integers: ";
     cin >> frac_3;
 
     cout << "You entered: " << frac_3 << endl;
@@ -35,9 +43,9 @@ int main()
     cout << "After decrementing: " << frac_3 << endl << endl;
 
     cout << "Now lets compare and do math on two fractions..." << endl;
-    cout << "Enter fraction #1: ";
+    cout << "Enter a fraction using integers; #1 (Ex. 2/3): ";
     cin >> frac_1;
-    cout << "Enter fraction #2: ";
+    cout << "Enter a fraction using integers; #2 (Ex. 2/3): ";
     cin >> frac_2;
     cout << endl;
     cout << frac_1 << " == " << frac_2 << " ?";
@@ -87,6 +95,82 @@ int main()
 
     frac_3 = frac_1 / frac_2;
     cout << frac_1 << " / " << frac_2 << " = " << frac_3 << endl << endl;
+
+    // End of part one---------------------------------------------------------------------
+    //-------------------------------------------------------------------------------------
+
+    // Part two, using doubles:
+
+    // Test the constructors:
+    Fraction<double> frac_4(1.2, 2.3);
+    Fraction<double> frac_5(2.4, 3.5);
+
+    cout << "Fraction #1: " << frac_4 << endl;
+    cout << "Fraction #2: " << frac_5 << endl << endl;
+
+    Fraction<double> frac_6;
+    cout << "Enter a fraction using double precision floats: ";
+    cin >> frac_6;
+
+    cout << "You entered: " << frac_6 << endl;
+    frac_6++;
+    cout << "After incrementing: " << frac_6 << endl;
+    frac_6--;
+    cout << "After decrementing: " << frac_6 << endl << endl;
+
+    cout << "Now lets compare and do math on two fractions..." << endl;
+    cout << "Enter a double precision float type; fraction #1 (Ex: 1.2/2.3) ";
+    cin >> frac_4;
+    cout << "Enter a double precision float type; fraction #2 (Ex: 1.2/2.3): ";
+    cin >> frac_5;
+    cout << endl;
+    cout << frac_4 << " == " << frac_5 << " ?";
+    if (frac_4 == frac_5)
+        cout << "  TRUE" << endl << endl;
+    else
+        cout << "  FALSE" << endl << endl;
+    
+    cout << frac_4 << " != " << frac_5 << " ?";
+    if (frac_4 != frac_5)
+        cout << "  TRUE" << endl << endl;
+    else
+        cout << "  FALSE" << endl << endl;
+
+    cout << frac_4 << " <= " << frac_5 << " ?";
+    if (frac_4 <= frac_5)
+        cout << "  TRUE" << endl << endl;
+    else
+        cout << "  FALSE" << endl << endl;
+
+    cout << frac_4 << " < " << frac_5 << " ?";
+    if (frac_4 < frac_5)
+        cout << "  TRUE" << endl << endl;
+    else
+        cout << "  FALSE" << endl << endl;
+
+    cout << frac_4 << " >= " << frac_5 << " ?";
+    if (frac_4 >= frac_5)
+        cout << "  TRUE" << endl << endl;
+    else
+        cout << "  FALSE" << endl << endl;
+
+    cout << frac_4 << " > " << frac_5 << " ?";
+    if (frac_4 > frac_5)
+        cout << "  TRUE" << endl << endl;
+    else
+        cout << "  FALSE" << endl << endl;
+
+    frac_6 = frac_4 + frac_5;
+    cout << frac_4 << " + " << frac_5 << " = " << frac_6 << endl << endl;
+
+    frac_6 = frac_4 - frac_5;
+    cout << frac_4 << " - " << frac_5 << " = " << frac_6 << endl << endl;
+
+    frac_6 = frac_4 * frac_5;
+    cout << frac_4 << " * " << frac_5 << " = " << frac_6 << endl << endl;
+
+    frac_6 = frac_4 / frac_5;
+    cout << frac_4 << " / " << frac_5 << " = " << frac_6 << endl << endl;
 
     return 0;
 }
