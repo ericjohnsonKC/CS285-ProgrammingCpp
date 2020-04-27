8c87930cd4a376ee4d0cd676e2bbeab7dfde3c11
eric <ericjohnson.kc@gmail.com>
2020-04-27 08:04:16 -0500

    The IDE is still showing errors so I've not tried to compile yet.  I've tried to add all the logic  that is needed, might still need to add some cout statements to provide context of what the program is doing at run time.

--- week-6/myVector.cpp
+++ week-6/myVector.cpp
@@ -16,13 +16,13 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 
 
 // Class myVector header...
-
 template <typename T>
 class myVector: public vector<T>{
 
@@ -36,8 +36,91 @@ class myVector: public vector<T>{
 };
 
 
-//  Class myVector implementation...
 
+//  Test program for the myVector class...
+int main(){
+    
+    // Define test vector(s)
+    myVector<string> nameList_1;
+    myVector<string> nameList_2;
+
+    //  Add values to the vector(s)
+    nameList_1.push_back("Ruth", "Gehrig", "Mantle", "Rivera", "Jeter", "Rizzuto", "Berra");
+
+    //  Test sort methods
+    nameList_1.bubbleSort();
+    
+    nameList_2.push_back("Ryan", "Koufax", "Martinez", "Maddux", "Johnson", "Feller", "Kershaw");
+
+    nameList_2.insertionSort();
+
+    myVector<string>::isSorted = true;
+
+    //  Test search methods
+    nameList_1.seqSearch("Bonds");
+    nameList_1.seqSearch("Ramirez");
+
+    nameList_2.binarySearch("Valenzuela");
+    nameList_2.binarySearch("Scherzer");
+
+    //  Print sorted vector using range
+    //  based for loop...
+    for (auto name : nameList_1)
+    {
+        cout << "Name list #1: " << endl;
+        cout << name << " ";
+    }
+
+    for (auto name : nameList_2)
+    {
+        cout << "Name list #2 : " << endl;
+        cout << name << " ";
+    }
+
+    //  Define new test vector(s)...
+    myVector<string> nameList_3;
+    myVector<string> nameList_4;
+
+    nameList_3.push_back("taco", "burrito", "nacho", "fajita", "quesadilla", "enchilada", "chorizo");
+    nameList_4.push_back("spaghetti", "fettucini", "linguini", "angel hair", "penne", "rigatoni", "ravioli");
+        
+    //  Define an iterator to each of
+    //  the above vector containers...
+    myVector<string>::iterator iter;
+
+    
+    //  Add values to the vector(s)...
+    nameList_3.push_back("carne asada");
+    nameList_4.push_back("pasta faziole");
+        
+    //  Test the STL sort method
+    sort(nameList_3.begin(), nameList_3.end());
+
+
+    //  Test the STL binary_search algorithm
+    binary_search(nameList_4.begin(), nameList_4.end(), "calzone");
+    binary_search(nameList_4.begin(), nameList_4.end(), "ravioli");
+
+    //  Print the resulting vector(s) using 
+    //  an iterator
+    for (auto iter : nameList_3)
+    {
+        cout << "Name list #3 after sorting: \n";
+        cout << iter << endl;
+    }
+
+    for (auto iter : nameList_4)
+    {
+        cout << "Name list #4 after sorting: \n";
+        cout << iter << endl;
+    }
+    
+    return 0;
+}
+
+
+
+//  Class myVector implementation...
 template <typename T>
 int myVector<T>::seqSearch(T searchItem){
     int loc;
@@ -45,7 +128,7 @@ int myVector<T>::seqSearch(T searchItem){
 
     loc = 0;
      
-    while (loc < listLength && !found)
+    while (loc < this->size() && !found)
         if (list[loc] == searchItem)
             found = true;
         else
@@ -59,36 +142,29 @@ int myVector<T>::seqSearch(T searchItem){
 
 template <typename T>
 int myVector<T>::binarySearch(T searchItem){
-    // Implement binary search
+    
+    int first = 0;
+    int last = this[this->size - 1];
+    int mid;
 
-// int binarySearch (const int list[], int listLength, int searchItem)
-// {
-//     int first = 0;
-//     int last = listLength - 1;
-//     int mid;
+    bool found = false;
 
-//     bool found = false;
+    while (first <= last && !found)
+    {
+        mid = (first + last) / 2;
 
-//     while (first <= last && !found)
-//     {
-//         mid = (first + last) / 2;
+        if (this[mid] == searchItem)
+            found = true;
+        else if (this[mid] > searchItem)
+            last = mid -1;
+        else
+            first = mid + 1;        
+    }
 
-//         if (list[mid] == searchItem)
-//             found = true;
-//         else if (list[mid] > searchItem)
-//             last = mid -1;
-//         else
-//             first = mid + 1;        
-//     }
-
-//     if (found)
-//         return mid;
-//     else
-//         return -1;   
-// }
-
-
-
+    if (found)
+        return mid;
+    else
+        return -1;   
 }
 
 
@@ -99,71 +175,37 @@ void myVector<T>::bubbleSort(){
     for (int iteration = 1; iteration < length; iteration++)
     {
         for (int index = 0; index < length - iteration; index++)
-            if (list[index] > list[index + 1])
+            if (this[index] > this[index + 1])
             {
-                temp = list[index];
-                list[index] = list[index + 1];
-                list[index + 1] = temp;
+                temp = this[index];
+                this[index] = this[index + 1];
+                this[index + 1] = temp;
             }
     }
 }
 
 template <typename T>
 void myVector<T>::insertionSort(){
-    // Implement insertion sort
 
+    int firstOutOfOrder, location;
+    int temp;
 
-// void insertionSort(int list[], int listLength){
-//     int firstOutOfOrder, location;
-//     int temp;
+    for (firstOutOfOrder = 1; firstOutOfOrder < this->size - 1; firstOutOfOrder++)
+        if (this[firstOutOfOrder] < this[firstOutOfOrder - 1])
+        {
+            temp = this[firstOutOfOrder];
+            location = firstOutOfOrder;
 
-//     for (firstOutOfOrder = 1; firstOutOfOrder < listLength; firstOutOfOrder++)
-//         if (list[firstOutOfOrder] < list[firstOutOfOrder -1])
-//         {
-//             temp = list[firstOutOfOrder];
-//             location = firstOutOfOrder;
+            do{
+                this[location] = this[location - 1];
+                location--;
+            }
+            while (location > 0 && this[location - 1] > temp);
 
-//             do{
-//                 list[location] = list[location -1];
-//                 location--;
-//             }
-//             while (location > 0 && list[location - 1] > temp);
-
-//             list[location] = temp;
-//         }
-// }
-
-
+            this[location] = temp;
+        }
 }
 
 
 
 
-//  Test program for the myVector class...
-
-int main(){
-    
-    // Define test vector(s)
-    myVector<string> nameList;
-
-    //  Add values to the vector(s)
-
-    //  Test sort methods
-
-    //  Test search methods
-
-    //  Print sorted vector using range
-    //  based for loop
-        //  Define new test vector(s)
-
-        //  Define an iterator to each of
-        //  the above vector containers
-
-        //  Add values to the vector(s)
-        //  Test the STL sort method
-        //  Test the STL binary_search algorithm
-        //  Print the resulting vector(s) using 
-        //  an iterator
-    
-    return 0;
-}
