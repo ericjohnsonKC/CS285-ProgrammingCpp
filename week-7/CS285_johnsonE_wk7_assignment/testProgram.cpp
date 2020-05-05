d503071161e2ee50222c17f982acb325a2bfc3f5
eric <ericjohnson.kc@gmail.com>
2020-05-05 12:14:00 -0500

    Ready to make first edits requrired by assignment instructions.  Starting in the unorderedLinkedList.h file.

---
+++ week-7/CS285_johnsonE_wk7_assignment/testProgram.cpp
@@ -0,0 +1,23 @@
+/*  Author: Eric Johnson
+    Date:   5/5/2020
+    Grantham University
+    CS285 Advanced Programming in C++
+    Week 7 Assignment
+
+    This assignment is on the implementation
+    of linked lists.
+*/
+
+#include <iostream>
+
+#include "unorderedLinkedList.h"
+
+using namespace std;
+
+
+int main(){
+
+
+
+    return 0;
+}
